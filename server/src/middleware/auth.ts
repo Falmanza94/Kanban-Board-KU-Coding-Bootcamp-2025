d0e602da9ba85072080d89843607842c4fed5159
Falmanza94 <fischer.almanza@gmail.com>
2025-04-01 14:03:04 -0500

    added several comments

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -7,8 +7,10 @@ interface JwtPayload {
 
 export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
   // TODO: verify the token exists and add the user data to the request object
-  const authHeader = req.headers.authorization;
 
+  //this line is trying to access the Authorization header
+  const authHeader = req.headers.authorization;
+  //this whole if statement checks whether the Authorization header is present.. if not then the response is a 401 error
   if (authHeader) {
     const token = authHeader.split(' ')[1];
     const secretKey = process.env.JWT_SECRET_KEY || '';
@@ -19,7 +21,8 @@ export const authenticateToken = (req: Request, res: Response, next: NextFunctio
       req.user = user as JwtPayload;
       return next();
     });
+    // this else statement is the error code which when invalid credentials are submitted the middleware sends a 401
   } else {
-    res.sendStatus(401);
+    res.sendStatus(401)
   }
 };
